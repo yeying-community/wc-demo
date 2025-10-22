5bb43740f82a6dfe891c2e4660591c5a7a916c15
yeying <yeying.community@gmail.com>
2025-10-22 23:47:32 +0800

    refactor sdk.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,4 +1,10 @@
-export * from './types';
-export * from './waku/client';
-export * from './crypto';
-export * from './utils/jwt';
+export * from './types/common';
+export * from './types/crypto';
+export * from './types/pairing';
+export * from './types/relay';
+export * from './types/session';
+
+export * from './client/dapp';
+export * from './client/wallet';
+
+export * from './utils/pairing-uri';
